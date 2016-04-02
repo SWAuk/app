94c30a10bb3c6d6d753119468f7dd8a75a3be238
Ollie Shotton <geekman92@gmail.com>
2016-04-02 01:36:59 +0100

    Added click events to CompetitorList

--- app/src/main/java/uk/co/swa/swapp/controller/CompetitionListActivity.java
+++ app/src/main/java/uk/co/swa/swapp/controller/CompetitionListActivity.java
@@ -97,9 +97,6 @@ public class CompetitionListActivity extends AppCompatActivity {
 
                 Log.d(getLocalClassName(), "Competition ListView Clicked, appID: " + appID);
 
-                Competition competition = god.getAppStore().getCompetition(appID);
-                competition.createCompetitionHeats(5);
-
                 Intent intent = new Intent(CompetitionListActivity.this,
                         CompetitorListActivity.class);
 
--- app/src/main/java/uk/co/swa/swapp/controller/CompetitorListActivity.java
+++ app/src/main/java/uk/co/swa/swapp/controller/CompetitorListActivity.java
@@ -3,54 +3,66 @@ package uk.co.swa.swapp.controller;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.List;
+
+import uk.co.swa.swapp.God;
 import uk.co.swa.swapp.R;
+import uk.co.swa.swapp.model.Competition;
+import uk.co.swa.swapp.model.CompetitionEntrant;
 
 public class CompetitorListActivity extends AppCompatActivity {
 
-    String[][] event_competitors = {
-            {}, {}
-    };
+    private God god;
+    private boolean isTeam;
+
+    private ArrayAdapter competitorListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
+        this.god = God.getInstance();
+
+        // get the competitionID passed from the activity calling us
+        long competitionID = getIntent().getLongExtra("competitionID", -1);
+        Log.d(getLocalClassName(), "competitionID: " + competitionID);
+
         setContentView(R.layout.activity_competitor_list);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Not implemented yet...", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        ListView compListView = (ListView) findViewById(R.id.competitorsListView);
+        Competition competition = this.god.getAppStore().getCompetition(competitionID);
 
-        // get the competitionID passed from the activity calling us
-        int competitionID = (int) getIntent().getLongExtra("competitionID", -1);
-        Log.d(getLocalClassName(), "competitionID: " + competitionID);
+        // TODO: this seems a bit of a hack checking it contains a String?
+        // check whether it is a team competition type or not.
+        this.isTeam = competition.getCompetitionType().toString().contains("Team");
 
-        if (competitionID > -1 && competitionID < event_competitors.length) {
-            String[] events = event_competitors[competitionID];
-            ArrayAdapter<String> eventsAdapter = new ArrayAdapter<String>(compListView.getContext(),
-                    android.R.layout.simple_list_item_1, events);
+        List<? extends CompetitionEntrant> competitionEntrantList =
+                this.god.getAppStore().getCompetitionEntrants(competition);
 
-            compListView.setAdapter(eventsAdapter);
-        } else {
-            // TODO: Show empty message
-        }
+        ListView competitorListView = (ListView) findViewById(R.id.competitorListView);
+
+        this.competitorListAdapter = new SwaObjectAdapter(this,
+                android.R.layout.simple_list_item_1, competitionEntrantList);
+
+        competitorListView.setAdapter(competitorListAdapter);
+
+        this.createFloatingActionButton();
+        competitorListView.setOnItemClickListener(this.onCompetitorListItemClick());
+        competitorListView.setOnItemLongClickListener(this.onCompetitorListItemLongClick());
+
     }
 
     @Override
@@ -61,6 +73,57 @@ public class CompetitorListActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void createFloatingActionButton() {
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(this.onFabClick());
+
+        if (this.isTeam) {
+            // set the Floating Action Button icon to group to indicate adding teams
+            fab.setImageDrawable(ContextCompat.getDrawable(this,
+                    R.drawable.ic_group_add_white_24dp));
+        }
+    }
+
+    private View.OnClickListener onFabClick() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Not implemented yet...", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        };
+    }
+
+    private AdapterView.OnItemClickListener onCompetitorListItemClick() {
+        return new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long appID) {
+                Snackbar.make(view, "Click not implemented yet...",
+                        Snackbar.LENGTH_LONG).setAction(null, null).show();
+            }
+        };
+    }
+
+    private AdapterView.OnItemLongClickListener onCompetitorListItemLongClick() {
+        return new EditDeleteDialogOnItemLongClick(this.onEditDeleteDialogClick());
+    }
+
+    private OnEditDeleteDialogClick onEditDeleteDialogClick() {
+        return new OnEditDeleteDialogClick() {
+            @Override
+            public void onEditClicked(AdapterView<?> parent, View view, int position, long appID) {
+                Snackbar.make(view, "Edit not implemented yet...",
+                        Snackbar.LENGTH_LONG).setAction(null, null).show();
+            }
+
+            @Override
+            public void onDeleteClicked(AdapterView<?> parent, View view, int position, long appID) {
+                Snackbar.make(view, "Delete not implemented yet...",
+                        Snackbar.LENGTH_LONG).setAction(null, null).show();
+            }
+        };
     }
 
 }

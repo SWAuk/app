646c1de865b57c024d9173448c47baa5bb2ed802
Ollie Shotton <geekman92@gmail.com>
2016-04-10 11:09:30 +0100

    Implemented the Heat object in the data model.

--- app/src/main/java/uk/co/swa/swapp/model/Heat.java
+++ app/src/main/java/uk/co/swa/swapp/model/Heat.java
@@ -1,11 +1,34 @@
 package uk.co.swa.swapp.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Heat implements SwaObject {
 
     long appID;
+    Competition competition;
+    List<? extends CompetitionEntrant> competitionEntrantList;
+    int duration;
 
-    public Heat(long appID) {
+    public Heat(long appID,
+                Competition competition,
+                int duration,
+                List<? extends CompetitionEntrant> competitionEntrants) {
         this.appID = appID;
+        this.competition = competition;
+        this.duration = duration;
+        this.competitionEntrantList = competitionEntrants;
+
+    }
+
+    public Heat (Competition competition,
+                 int duration,
+                 List<? extends CompetitionEntrant> competitionEntrants) {
+        this(-1, competition, duration, competitionEntrants);
+    }
+
+    public Heat (Competition competition, int duration) {
+        this(-1, competition, duration, new ArrayList<CompetitionEntrant>());
     }
 
     @Override
